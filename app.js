f57d9ffbe91e9bb58a05bc7d34bd8b651b4cea29
maitrygajiwala <maitry22gajiwala@gmail.com>
2024-10-24 12:06:06 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const people = [
+    {
+        name: 'bob',
+        age: 23,
+        position: 'developer',
+    },
+    {
+        name: 'suzy',
+        age: 24,
+        position: 'designer',
+    },
+    {
+        name: 'anna',
+        age: 24,
+        position: 'engineer',
+    }
+];
+
+const ages = people.map(() => {})
+
+console.log("Hello World");
+let name = 'adul';
+console.log(name)
+
